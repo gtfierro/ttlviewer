395e66b667d9b33d67c114cf21e75c5436ae0aa0
Gabe Fierro <gtfierro225@gmail.com>
2016-11-11 11:56:47 -0800

    support uploading from a url

--- server/main.go
+++ server/main.go
@@ -33,25 +33,41 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func upload(rw http.ResponseWriter, req *http.Request) {
+	var (
+		pdf []byte
+		dot []byte
+		err error
+	)
 	defer req.Body.Close()
 	if req.Method == "GET" {
 		index(rw, req)
 		return
 	}
-	if err := req.ParseMultipartForm(5 << 20); err != nil {
-		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
-		return
+	// first test for URL
+	url := req.FormValue("URL")
+	if url != "" { // then we have a url!
+		res, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		pdf, dot, err = ttl.RunFile(res.Body, false)
+	} else {
+		if err := req.ParseMultipartForm(5 << 20); err != nil {
+			rw.WriteHeader(500)
+			rw.Write([]byte(err.Error()))
+			return
+		}
+		file, _, err := req.FormFile("uploadfile")
+		if err != nil {
+			rw.WriteHeader(500)
+			rw.Write([]byte(err.Error()))
+			return
+		}
+		defer file.Close()
+		pdf, dot, err = ttl.RunFile(file, false)
 	}
-	file, _, err := req.FormFile("uploadfile")
-	if err != nil {
-		rw.WriteHeader(500)
-		rw.Write([]byte(err.Error()))
-		return
-	}
-	defer file.Close()
 	renderOption := req.FormValue("render")
-	pdf, dot, err := ttl.RunFile(file, false)
 	if err != nil {
 		rw.WriteHeader(500)
 		rw.Write([]byte(err.Error()))
